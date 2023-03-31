1f0463d2031e25b4ee4445c1851ff95733c783ba
zekeriyabasan <zekeriyabasan@outlook.com>
2023-03-31 18:25:46 +0300

    -Added Services Layer added contracts for BookManager and ServiceManager

---
+++ Services/BookManager.cs
@@ -0,0 +1,64 @@
+﻿using Entities.Models;
+using Repositories.Contracts;
+using Services.Contracts;
+
+namespace Services
+{
+    public class BookManager : IBookService
+    {
+        private readonly IRepositoryManager _manager;
+
+        public BookManager(IRepositoryManager manager)
+        {
+            _manager = manager;
+        }
+
+        public Book CreateABook(Book book)
+        {
+            _manager.Book.CreateABook(book);
+            _manager.Save();
+            return book;
+        }
+
+        public void DeleteABook(int id, bool trackChanges)
+        {
+            var exist = _manager.Book.GetABook(id,false);
+            if (exist == null)
+            {
+                throw new Exception($"book is not absent for {id}");
+
+            }
+            _manager.Book.DeleteABook(exist);
+            _manager.Save();
+        }
+
+        public Book GetABook(int id, bool trackChanges)
+        {
+            return _manager.Book.GetABook(id,trackChanges);
+        }
+
+        public IEnumerable<Book> GetAllBooks(bool trackChanges)
+        {
+            return _manager.Book.GetAllBook(trackChanges);
+        }
+
+        public void UpdateABook(int id, Book book, bool trackChanges)
+        {
+            var exist = _manager.Book.GetABook(id, false);
+            if (exist == null)
+            {
+                throw new Exception($"book is not absent for {id}");
+            }
+            if(exist.Id != id)
+                throw new Exception($"not matched ids {id}!={exist.Id}");
+
+            if(book is null)
+                throw new ArgumentNullException(nameof(book));
+
+            exist.Name=book.Name;
+            exist.Price=book.Price;
+            _manager.Book.UpdateABook(exist);
+            _manager.Save();
+        }
+    }
+}
---
+++ Services/Contracts/IBookService.cs
@@ -0,0 +1,18 @@
+﻿using Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Contracts
+{
+    public interface IBookService
+    {
+        IEnumerable<Book> GetAllBooks(bool trackChanges);
+        Book GetABook(int id, bool trackChanges);
+        Book CreateABook(Book book);
+        void UpdateABook(int id, Book book, bool trackChanges);
+        void DeleteABook(int id, bool trackChanges);
+    }
+}
