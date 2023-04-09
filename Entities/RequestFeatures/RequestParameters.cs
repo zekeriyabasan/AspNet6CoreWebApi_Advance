483af9334e6714375114a589510319fe664fb506
zekeriyabasan <zekeriyabasan@outlook.com>
2023-04-09 13:48:10 +0300

    created base class for request by pagination

---
+++ Entities/RequestFeatures/RequestParameters.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.RequestFeatures
+{
+    public abstract class RequestParameters // base class olarak tanımlayalım. Herkes için ortak (?pageNumber&pageSize)
+    {
+        const int maxPageSize = 50;
+
+        // Auto-implemented property
+        public int PageNumber { get; set; }
+
+        // FULL-property
+        public int _pageSize;
+        public int PageSize
+        {
+            get { return _pageSize; }
+            set { _pageSize = value > maxPageSize ? maxPageSize : value; } // gönderilen _pageSize max dan büyükse maxPageSize ı dön değilse kendisini
+        
+        }
+
+    }
+}
