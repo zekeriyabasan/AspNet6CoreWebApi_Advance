c99fea1a62652c496d446de70af347a135902549
zekeriyabasan <zekeriyabasan@outlook.com>
2023-04-20 15:40:11 +0300

    edit

--- Presentation/ActionFilters/ValidateMediaTypeAttribute.cs
+++ Presentation/ActionFilters/ValidateMediaTypeAttribute.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Entities.Exceptions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Net.Http.Headers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +10,29 @@ using System.Threading.Tasks;
 
 namespace Presentation.ActionFilters
 {
-    internal class ValidateMediaTypeAttribute
+    public class ValidateMediaTypeAttribute : ActionFilterAttribute
     {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            var acceptHeaderPresent = context
+                .HttpContext
+                .Request
+                .Headers.ContainsKey("Accept");
+
+            if (!acceptHeaderPresent)
+            {
+                context.Result = new BadRequestObjectResult($"Accept header is missing!"); return;// Accept header eklenmemişse burada kontrolü
+            }
+               
+
+            var mediaType = context.HttpContext.Request.Headers["Accept"].FirstOrDefault();
+
+            if (MediaTypeHeaderValue.TryParse(mediaType, out MediaTypeHeaderValue? outMediaType)) //out(parametre modifier dır) yani outMediaType değeri tryparse sonrası.
+            {
+                context.Result = new BadRequestObjectResult($"Media type not present." + $"Please add Accept header required media type"); // istediğimiz tip değilse kontorl
+                return;
+            } // aklenen tip mevcut değil (yanlış tip)
+
+            context.HttpContext.Items.Add("AcceptHeaderMediaType", outMediaType);
+        }
     }
-}
