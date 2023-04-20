33306c2b5065fe1d0892daf21e881e62fa8b47cb
zekeriyabasan <zekeriyabasan@outlook.com>
2023-04-20 14:51:46 +0300

    prepared Entities for link and ShaperData

---
+++ Entities/LinkModels/Link.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.LinkModels
+{
+    public class Link
+    {
+        public string? Href { get; set; } // Hyper refrence
+        public string? Rel { get; set; } // Relation
+        public string? Method { get; set; }
+        public Link() // serileştirme için gerekli
+        {
+            
+        }
+        public Link(string? href, string? rel, string? method) // full
+        {
+            Href = href;
+            Rel = rel;
+            Method = method;
+        }
+    }
+
+    public class LinkResourceBase
+    {
+        public LinkResourceBase()
+        {
+            
+        }
+        public List<Link> Links { get; set; } = new List<Link>(); // en çok yapılan hata ref tipli prop ın ınstance'ını vermemek
+    }
+}
---
+++ Entities/Models/ShapedEntity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Models
+{
+    public class ShapedEntity
+    {
+        public int Id { get; set; }
+        public Entity Entity { get; set; } // en sık yapılan hata ref tipli prop ın ref ını vermemek (ya ctor da yada tanımlandığı yerde verilmeli)
+        public ShapedEntity()
+        {
+            Entity = new Entity();   
+        }
+    }
+}
