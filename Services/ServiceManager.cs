edfddab443333bbe6ec92bb38bd85f33ec0f476e
zekeriyabasan <zekeriyabasan@outlook.com>
2023-06-11 16:46:29 +0300

    ServiceManger refactoring

--- Services/ServiceManager.cs
+++ Services/ServiceManager.cs
@@ -1,5 +1,8 @@
 ﻿using AutoMapper;
 using Entities.DataTransferObjects;
+using Entities.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
 using Repositories.Contracts;
 using Services.Contracts;
 using System;
@@ -13,12 +16,24 @@ namespace Services
     public class ServiceManager : IServiceManager
     {
         private readonly Lazy<IBookService> _bookService;
-
-        public ServiceManager(IRepositoryManager repositoryManager, ILoggerService logger, IMapper mapper, IBookLinks bookLinks)
+        private readonly Lazy<IAuthenticationService> _authenticationService;
+        public ServiceManager(
+            IRepositoryManager repositoryManager, 
+            ILoggerService logger, 
+            IMapper mapper,
+            IConfiguration configuration,
+            IBookLinks bookLinks,   
+            UserManager<User> userManager)
         {
-            _bookService = new Lazy<IBookService>(()=>new BookManager(repositoryManager, logger, mapper, bookLinks));
+            _bookService = new Lazy<IBookService>(() => new BookManager(repositoryManager, logger, mapper, bookLinks));
+
+            _authenticationService = new Lazy<IAuthenticationService>(() =>
+            new AuthenticationManager (logger,mapper,userManager,configuration));
+            
         }
 
         public IBookService BookService => _bookService.Value;
+
+        public IAuthenticationService AuthenticationService => _authenticationService.Value;
     }
 }
