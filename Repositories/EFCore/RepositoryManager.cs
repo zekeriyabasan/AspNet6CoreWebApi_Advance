78548fa4acf990b3f145358813bae9fb7035abc0
zekeriyabasan <zekeriyabasan@outlook.com>
2023-07-01 18:41:49 +0300

    perpared RepositoryManager for Category

--- Repositories/EFCore/RepositoryManager.cs
+++ Repositories/EFCore/RepositoryManager.cs
@@ -10,14 +10,17 @@ namespace Repositories.EFCore
     public class RepositoryManager : IRepositoryManager
     {
         private readonly RepositoryContext _context;
-        private readonly Lazy<IBookRepository> _bookRepository; // Lazy loading ile nesne ancak ve ancak kullanıldığı anda newlenecek
+        private readonly Lazy<IBookRepository> _bookRepository; //nesne ancak ve ancak kullanıldığı anda new lenecek
+        private readonly Lazy<ICategoryRepository> _categoryRepository;
         public RepositoryManager(RepositoryContext context)
         {
             _context = context;
             _bookRepository = new Lazy<IBookRepository>(()=> new BookRepository(_context));
+            _categoryRepository = new Lazy<ICategoryRepository>(()=> new CategoryRepository(_context));
         }
 
         public IBookRepository Book => _bookRepository.Value;
+        public ICategoryRepository Category => _categoryRepository.Value;
 
         public async Task SaveAsync()
         {
